c0bb1d6c9871e2f545e60aa9e0ad8bb392f83bb7
dzmih <daniil.nikolenko2006@gmail.com>
2026-06-29 12:33:55 +0300

    Fix: use self.pc instead of pc in run method

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-class Execution:
+class VirtualMachine:
     def __init__(self, default_actions=None):
         self.stack = []
         self.memory = [None] * 1024
+        self.pc = 0
+        self.running = True
         self.default_actions = default_actions or {
         "push": self.push,
         "pop": self.pop,
@@ -15,7 +17,23 @@ class Execution:
         "add": self.add,
         "store": self.store,
         "load": self.load,
+        "jump": self.jump,
+        "halt": self.halt,
     }
+        
+    #Private
+    def _push_stack(self, value):
+        try:
+            value = int(value)
+        except ValueError:
+            pass
+        self.stack.append(value)
+
+    def _pop_stack(self):
+        return self.stack.pop() 
+
+    
+
         
     def process(self, action, *args, custom_actions=None, **kwargs):
         actions = custom_actions or self.default_actions
@@ -27,9 +45,11 @@ class Execution:
         except ValueError:
             pass
         self.stack.append(value)
+        self.pc += 1
     
     def pop(self):
-        return self.stack.pop()
+        self.pc += 1
+        return self.stack.pop() 
     
     def prnt(self, option):
         option = option.replace(" ", "")
@@ -37,14 +57,17 @@ class Execution:
             print(self.stack)
         if option.lower() == "memory":
             print(self.memory)
+        self.pc += 1
 
     def swap(self):
         if len(self.stack) >= 2:
             self.stack[-1], self.stack[-2] = self.stack[-2], self.stack[-1]
+        self.pc += 1
 
     def dup(self):
         if self.stack:
             self.stack.append(self.stack[-1])
+        self.pc += 1
 
     def mul(self):
         if not self.stack:
@@ -53,6 +76,7 @@ class Execution:
         for i in range(1, len(self.stack)):
             t *= self.stack[i]
         self.stack = [t]
+        self.pc += 1
 
     def div(self):
         if not self.stack:
@@ -61,15 +85,14 @@ class Execution:
         for i in range(1, len(self.stack)):
             t //= self.stack[i]
         self.stack = [t]
+        self.pc += 1
         
     def run(self, program:list[tuple]):
-        pc = 0
-        while pc < len(program):
-            if len(program[pc]) > 1:
-                self.process(program[pc][0], program[pc][1])
-            if len(program[pc]) == 1:
-                self.process(program[pc][0])
-            pc += 1
+        while self.pc < len(program) and self.running:
+            if len(program[self.pc]) > 1:
+                self.process(program[self.pc][0], program[self.pc][1])
+            if len(program[self.pc]) == 1:
+                self.process(program[self.pc][0])
 
     def add(self):
         if not self.stack:
@@ -78,6 +101,7 @@ class Execution:
         for i in range(1, len(self.stack)):
             t += self.stack[i]
         self.stack = [t]
+        self.pc += 1
 
     def repl(self):
         while True:
@@ -97,27 +121,33 @@ class Execution:
 
     def store(self, address):
         address = int(address)
-        value = self.pop()
+        value = self._pop_stack()
         self.memory[address] = value
+        self.pc += 1
 
     def load(self, address):
         address = int(address)
         value = self.memory[address]
-        self.push(value)
+        self._push_stack(value)
+        self.pc += 1
     
     def jump(self, index):
-        pass
+        self.pc = index
+    
+    def halt(self):
+        self.running = False
 
-main_vm = Execution()
 
-# program = [
-#     ("push", 5),
-#     ("push", 10),
-#     ("push", 10),
-#     ("push", 6),
-#     ("mul",),
-#     ("prnt",)
-# ]
+main_vm = VirtualMachine()
 
-# main_vm.repl()
+program = [
+    ("push", 5),
+    ("push", 10),
+    ("add",),
+    ("prnt", "stack"),
+    ("jump", 0),
+    ("halt",)
+]
+
+main_vm.run(program)
 
